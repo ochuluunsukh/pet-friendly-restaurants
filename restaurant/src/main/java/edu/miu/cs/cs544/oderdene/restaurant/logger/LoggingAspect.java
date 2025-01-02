a79ca5cffb77d416b1c6a4c74146de763c39028f
Odkoo Ch <odkoo@Odkoos-MacBook-Air.local>
2025-01-02 14:26:27 -0600

    update apsectj

--- restaurant/src/main/java/edu/miu/cs/cs544/oderdene/restaurant/logger/LoggingAspect.java
+++ restaurant/src/main/java/edu/miu/cs/cs544/oderdene/restaurant/logger/LoggingAspect.java
@@ -12,20 +12,38 @@ public class LoggingAspect {
     private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
     @Before("execution(* edu.miu.cs.cs544.oderdene.restaurant.service.*.*(..))")
-    public void logBefore() {
-        logger.info("Executing method...");
+    public void logServiceBefore() {
+        logger.info("Executing services method...");
     }
 
     @After("execution(* edu.miu.cs.cs544.oderdene.restaurant.service.*.*(..))")
-    public void logAfter() {
-        logger.info("Method execution completed.");
+    public void logServiceAfter() {
+        logger.info("Services method execution completed.");
     }
 
     @Around("execution(* edu.miu.cs.cs544.oderdene.restaurant.service.*.*(..))")
-    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        logger.info("Method {} is about to be executed.", joinPoint.getSignature().getName());
+    public Object logServiceAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        logger.info("Services method {} is about to be executed.", joinPoint.getSignature().getName());
         Object result = joinPoint.proceed();
-        logger.info("Method {} execution completed.", joinPoint.getSignature().getName());
+        logger.info("Services method {} execution completed.", joinPoint.getSignature().getName());
+        return result;
+    }
+
+    @Before("execution(* edu.miu.cs.cs544.oderdene.restaurant.controller.*.*(..))")
+    public void logControllerBefore() {
+        logger.info("Executing controller method...");
+    }
+
+    @After("execution(* edu.miu.cs.cs544.oderdene.restaurant.controller.*.*(..))")
+    public void logControllerAfter() {
+        logger.info("Controller method execution completed.");
+    }
+
+    @Around("execution(* edu.miu.cs.cs544.oderdene.restaurant.controller.*.*(..))")
+    public Object logControllerAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        logger.info("Controller method {} is about to be executed.", joinPoint.getSignature().getName());
+        Object result = joinPoint.proceed();
+        logger.info("Controller ethod {} execution completed.", joinPoint.getSignature().getName());
         return result;
     }
 }
